a11303d28a086373695dc168ebcd4e4644bea398
Sourabh <sourabhrohilla@gmail.com>
2017-08-24 19:05:35 +0530

    modified R scripts to take new test data

--- session-1/R/intrusion_binary_class.R
+++ session-1/R/intrusion_binary_class.R
@@ -7,44 +7,46 @@ sapply(load.libraries, require, character = TRUE)
 
 ####### Reading the data from csv #########
 # Enter your own path containing the dataset.
-file_path  <- ('~/Downloads/kddcup.data_10_percent1.csv')
-input_data <- read.csv(file_path,stringsAsFactors = FALSE,header = TRUE)
+train_file_path  <- ('~/Desktop/hands-on/session_1_data_train.csv')
+test_file_path  <- ('~/Desktop/hands-on/session_1_data_test.csv')
+train_data <- read.csv(train_file_path,stringsAsFactors = FALSE,header = TRUE)
+test_data <- read.csv(test_file_path,stringsAsFactors = FALSE,header = TRUE)
 
 #####Structure of data ######
-dim(input_data)
-str(input_data)
+dim(train_data)
+str(train_data)
 
 
 ######Removing duplicates from data####
-dim(unique(input_data))
-input_data <- unique(input_data)
+dim(unique(train_data))
+train_data <- unique(train_data)
 
 ###### Check for Missing data ########
-cols_with_missing_vals <- sapply(input_data, function(x) sum(is.na(x)/nrow(input_data))) 
+cols_with_missing_vals <- sapply(train_data, function(x) sum(is.na(x)/nrow(train_data))) 
 
 #####Grouping all attacks together##############
-input_data$label[!input_data$label %in% c('normal.')] <- 'attacks'
+train_data$label[!train_data$label %in% c('normal.')] <- 'attacks'
 
 ######Cleaning label data ###
-input_data$label[input_data$label %in% c('normal.')] <- 'normal'
+train_data$label[train_data$label %in% c('normal.')] <- 'normal'
 
 ### Converting columns to factors #### 
 cols_to_factors             <- c('protocol_type','service','flag','land','logged_in','is_host_login','is_guest_login','label')
-input_data[cols_to_factors] <- lapply(input_data[cols_to_factors],factor)
+train_data[cols_to_factors] <- lapply(train_data[cols_to_factors],factor)
 
 ###### Distribution of the class variable ###########
-label_info <- table(input_data$label)
+label_info <- table(train_data$label)
 barplot(label_info,ylim=c(0,85000), xlab="Label", ylab="Frequency", col="black",
         main="Label Distribution")
 
 ####Distribution of features with class variable######
-categorical_cols <- names(input_data)[ sapply(input_data, is.factor)]
-categorical_cols <- categorical_cols[!which(categorical_cols %in%  c("label"))]
-numeric_cols     <- names(input_data)[ sapply(input_data, is.numeric)]
+categorical_cols <- names(train_data)[ sapply(train_data, is.factor)]
+categorical_cols <- categorical_cols[-which(categorical_cols %in%  c("label"))]
+numeric_cols     <- names(train_data)[sapply(train_data, is.numeric)]
 
 for(col_name in categorical_cols)
 {
-  counts <- table(input_data$label,input_data[[col_name]])
+  counts <- table(train_data$label,train_data[[col_name]])
   barplot(counts,xlab=col_name,col=c("red", "lightblue"),
           beside=TRUE,
           ylab ="frequency", main = col_name)
@@ -53,60 +55,74 @@ for(col_name in categorical_cols)
 
 for (col_name in numeric_cols)
 {
-  minm<-min(input_data[[col_name]])  
-  maxm<-max(input_data[[col_name]])
-  boxplot(input_data[[col_name]]~input_data$label, col = "lightblue", xlab=col_name, ylab="frequency", main = col_name)
+  # minm<-min(train_data[[col_name]])  
+  # maxm<-max(train_data[[col_name]])
+  boxplot(train_data[[col_name]]~train_data$label, col = "lightblue", xlab=col_name, 
+          ylab="frequency", main = col_name)
 }
 
 #### Columns to be Removed######
-input_data <- input_data [ ,-which(names(input_data) %in% c('is_host_login','num_outbound_cmds','service'))]
+train_data <- train_data [ ,-which(names(train_data) %in% c(
+                  'is_host_login','num_outbound_cmds','service'))]
 
 #### Correlation matrix #####
-numeric_cols      <- names(input_data)[sapply(input_data, is.numeric)]
-correlationMatrix <- cor(input_data[,numeric_cols])
-relevant_correlations <- as.matrix(subset(melt(correlationMatrix, na.rm = TRUE), abs(value) > 0.8 & abs(value) <1))
+numeric_cols      <- names(train_data)[sapply(train_data, is.numeric)]
+correlationMatrix <- cor(train_data[,numeric_cols])
+relevant_correlations <- as.matrix(subset(melt(correlationMatrix, na.rm = TRUE), 
+                                          abs(value) > 0.8 & abs(value) <1))
 
-######Creating train-test split using stratified sampling########### 
-training_data_identifier      <- createDataPartition(y = input_data$label, p= 0.7, list = FALSE)
-training                      <- as.data.frame(input_data[training_data_identifier,])
-testing                       <- as.data.frame(input_data[-training_data_identifier,])
-test_label                    <- testing$label
-testing                       <- testing[-which(names(testing) %in% c('label'))]
+######Some preprocessing on test dataset.########### 
+test_data$label[!test_data$label %in% c('normal.')] <- 'attacks'
+test_data$label[test_data$label %in% c('normal.')] <- 'normal'
+### Converting columns to factors #### 
+cols_to_factors             <- c('protocol_type','service','flag','land','logged_in','is_host_login','is_guest_login','label')
+test_data[cols_to_factors] <- lapply(test_data[cols_to_factors],factor)
+#### Columns to be Removed######
+test_data <- test_data [ ,-which(names(test_data) %in% c(
+  'is_host_login','num_outbound_cmds','service'))]
+X_test                       <- test_data[-which(names(test_data) %in% c('label'))]
+y_test                       <- test_data$label
+
 
 #####Building Naive-bayes Model#########
-nb_model    <- naive_bayes(training$label ~ ., data=training)
-nb_predictions     <- predict(nb_model,testing)
-nb_cnmatrix <- confusionMatrix(table(nb_predictions,test_label))
+nb_model    <- naive_bayes(train_data$label ~ ., data=train_data)
+nb_predictions     <- predict(nb_model,X_test)
+nb_cnmatrix <- confusionMatrix(table(nb_predictions,y_test))
 
 ####Building logistic-regression Model #############
-lr_model    <- glm(training$label ~. ,data=training,family = binomial(logit))
-lr_predictions     <- predict.glm(lr_model,testing,type = 'response')
-lr_cnmatrix <- table(test_label,lr_predictions > 0.5)
+lr_model    <- glm(train_data$label ~. ,data=train_data,family = binomial(logit))
+lr_predictions     <- predict.glm(lr_model,X_test,type = 'response')
+lr_cnmatrix <- table(y_test,lr_predictions > 0.5)
  
 
 ####Building Random Forest Model#######
-rf_model    <- randomForest(training$label ~ ., data=training,importance=TRUE,ntree=100)
-rf_predictions     <- predict(rf_model,testing)
-rf_cnmatrix <- confusionMatrix(table(rf_predictions,test_label))
+rf_model    <- randomForest(train_data$label ~ ., data=train_data,importance=TRUE,ntree=100)
+rf_predictions     <- predict(rf_model,X_test)
+rf_cnmatrix <- confusionMatrix(table(rf_predictions,y_test))
 
 ###Building SVM #####
-svm_model <- svm(training$label ~ ., data=training)
-svm_pred     <- predict(svm_model,testing)
-svm_cnmatrix <- confusionMatrix(table(svm_pred,test_label))
+svm_model <- svm(train_data$label ~ ., data=train_data)
+svm_pred     <- predict(svm_model,X_test)
+svm_cnmatrix <- confusionMatrix(table(svm_pred,y_test))
 
 ####Building Decision Tree #####
-dtree_model<-rpart(training$label ~ ., data=training)
+dtree_model<-rpart(train_data$label ~ ., data=train_data)
 rpart.plot(dtree_model)
-dtree_pred <- predict(dtree_model,testing, type = "class")
-dtree_cnmatrix <- confusionMatrix(table(dtree_pred,test_label))
+dtree_pred <- predict(dtree_model,X_test, type = "class")
+dtree_cnmatrix <- confusionMatrix(table(dtree_pred,y_test))
 
 
 
 ##Hyperparameter tuning with GridSearchCV to tune random forest
-control <- trainControl(method="repeatedcv", number=10, repeats=3)
-grid <- expand.grid(mtry=c(6,8,10))
-rf_model <- train(training$label ~. ,data=training,family = "binomial",
-                  method="rf", trControl=control, tuneGrid=grid)
+control <- trainControl(method="repeatedcv", number=3, repeats=3,
+                        classProbs = TRUE,
+                        summaryFunction  = twoClassSummary,search="grid")
+grid <- expand.grid(mtry=c(6,8))
+rf_model <- train(train_data$label ~. ,data=train_data,family = "binomial",
+                  method="rf", metric="ROC",
+                  trControl=control, tuneGrid=grid)
+rf_predictions     <- predict(rf_model,X_test)
+rf_cnmatrix <- confusionMatrix(table(rf_predictions,y_test))
 
 ##Balance class data
 #1. Adjust class weights
@@ -117,5 +133,3 @@ mod1 <- train(training$label ~. ,data=training,
                                      trials = c(1:10, (1:5)*10),
                                      cost = 1:10),
               trControl = control)
-
-#2. Change decision threshold
--- session-1/R/intrusion_multi_class.R
+++ session-1/R/intrusion_multi_class.R
@@ -6,48 +6,41 @@ sapply(load.libraries, require, character = TRUE)
 
 ####### Reading the data from csv #########
 # Enter your own path containing the dataset.
-file_path  <- ('/home/karan/bangalore_conf/kddcup.data_10_percent1.csv')
-input_data <- read.csv(file_path,stringsAsFactors = FALSE,header = TRUE)
+train_file_path  <- ('~/Desktop/hands-on/session_1_data_train.csv')
+test_file_path  <- ('~/Desktop/hands-on/session_1_data_test.csv')
+train_data <- read.csv(train_file_path,stringsAsFactors = FALSE,header = TRUE)
+test_data <- read.csv(test_file_path,stringsAsFactors = FALSE,header = TRUE)
 
 #####Structure of data ######
-dim(input_data)
-str(input_data)
+dim(train_data)
+str(train_data)
 
 ######Removing duplicates from data####
-dim(unique(input_data))
-input_data <- unique(input_data)
+dim(unique(train_data))
+train_data <- unique(train_data)
 
 ###### Check for Missing data ########
-cols_with_missing_vals <- sapply(input_data, function(x) sum(is.na(x)/nrow(input_data))) 
+cols_with_missing_vals <- sapply(train_data, function(x) sum(is.na(x)/nrow(train_data))) 
 
-#####Grouping attack labels into sub categories#########
-input_data$label[input_data$label %in% c('back.','land.','neptune.','pod.','smurf.',
-                                         'teardrop.')] <- 'dos'
-input_data$label[input_data$label %in% c('buffer_overflow.','loadmodule.','perl.','rootkit.')] <- 'utr'
-input_data$label[input_data$label %in% c('ftp_write.','guess_passwd.','imap.','multihop.'
-                                         ,'phf.','spy.','warezclient.','warezmaster.')] <- 'rtl'
-input_data$label[input_data$label %in% c('satan.','ipsweep.','nmap.','portsweep.')] <- 'probes'
-
-######Cleaning label data ###
-input_data$label[input_data$label %in% c('normal.')] <- 'normal'
+train_data$label = mapply(get_attack_type_label,train_data$label)
 
 ### Converting columns to factors #### 
 cols_to_factors             <- c('protocol_type','service','flag','land','logged_in','is_host_login','is_guest_login','label')
-input_data[cols_to_factors] <- lapply(input_data[cols_to_factors],factor)
+train_data[cols_to_factors] <- lapply(train_data[cols_to_factors],factor)
 
 ###### Distribution of the class variable ###########
-label_info <- table(input_data$label)
+label_info <- table(train_data$label)
 barplot(label_info,ylim=c(0,85000), xlab="Label", ylab="Frequency", col="black",
         main="Label Distribution")
 
 ####Distribution of features with class variable######
-categorical_cols <- names(input_data)[ sapply(input_data, is.factor)]
+categorical_cols <- names(train_data)[ sapply(train_data, is.factor)]
 categorical_cols <- categorical_cols[-which(categorical_cols %in%  c("label"))]
-numeric_cols     <- names(input_data)[ sapply(input_data, is.numeric)]
+numeric_cols     <- names(train_data)[ sapply(train_data, is.numeric)]
 
 for(col_name in categorical_cols)
 {
-  counts <- table(input_data$label,input_data[[col_name]])
+  counts <- table(train_data$label,train_data[[col_name]])
   barplot(counts,xlab=col_name,col=c("red", "lightblue"),
           beside=TRUE,
           ylab ="frequency", main = col_name)
@@ -56,51 +49,75 @@ for(col_name in categorical_cols)
 
 for (col_name in numeric_cols)
 {
-  minm<-min(input_data[[col_name]])  
-  maxm<-max(input_data[[col_name]])
-  boxplot(input_data[[col_name]]~input_data$label, col = "lightblue", xlab=col_name, ylab="frequency", main = col_name)
+  boxplot(train_data[[col_name]]~train_data$label, col = "lightblue", xlab=col_name, 
+          ylab="frequency", main = col_name)
 }
 
 #### Columns to be Removed######
-input_data <- input_data [ ,-which(names(input_data) %in% c('is_host_login','num_outbound_cmds','service'))]
+train_data <- train_data [ ,-which(names(train_data) %in% c('is_host_login','num_outbound_cmds','service'))]
 
 #### Correlation matrix #####
-numeric_cols      <- names(input_data)[sapply(input_data, is.numeric)]
-correlationMatrix <- cor(input_data[,numeric_cols])
+numeric_cols      <- names(train_data)[sapply(train_data, is.numeric)]
+correlationMatrix <- cor(train_data[,numeric_cols])
 relevant_correlations <- as.matrix(subset(melt(correlationMatrix, na.rm = TRUE), abs(value) > 0.8 & abs(value) <1))
 
-######Creating train-test split using stratified sampling########### 
-intot      <- createDataPartition(y = input_data$label, p= 0.7, list = FALSE)
-training   <- as.data.frame(input_data[intot,])
-testing    <- as.data.frame(input_data[-intot,])
-test_label <- testing$label
-testing    <- testing[-which(names(testing) %in% c('label'))]
 
+
+######Some preprocessing on test dataset.########### 
+get_attack_type_label <- function(attack_name){
+  if(attack_name %in% c('back.','land.','neptune.','pod.','smurf.','teardrop.')){
+    return('dos')
+  }
+  else if(attack_name %in% c('buffer_overflow.','loadmodule.','perl.','rootkit.')){
+    return('utr')
+  }
+  else if(attack_name %in% c('ftp_write.','guess_passwd.','imap.','multihop.',
+                             'phf.','spy.','warezclient.','warezmaster.')){
+    return('rtl')
+  }
+  else if(attack_name %in% c('satan.','ipsweep.','nmap.','portsweep.')){
+    return('probes')
+  }
+  else if(attack_name %in% c('normal.')){
+    return('normal')
+  }
+  return('others')
+}
+
+test_data$label = mapply(get_attack_type_label,test_data$label)
+cols_to_factors             <- c('protocol_type','service','flag','land','logged_in','is_host_login','is_guest_login','label')
+test_data[cols_to_factors] <- lapply(test_data[cols_to_factors],factor)
+test_data <- test_data [ ,-which(names(test_data) %in% c('is_host_login','num_outbound_cmds','service'))]
+X_test                       <- test_data[-which(names(test_data) %in% c('label'))]
+y_test                       <- test_data$label
+
+
+#TODO : Handle confusion matrix when others is missing in training data.
 #####Building Naive-bayes Model#########
-nb_model    <- naive_bayes(training$label ~ ., data=training)
-nb_pred     <- predict(nb_model,testing)
-nb_cnmatrix <- confusionMatrix(table(nb_pred,test_label))
+nb_model    <- naive_bayes(train_data$label ~ ., data=train_data)
+nb_pred     <- predict(nb_model,X_test)
+nb_cnmatrix <- confusionMatrix(table(nb_pred,y_test))
 
 ####Building logistic-regression Model #############
-lr_model    <- glm(training$label ~. ,data=training,family = binomial(logit))
-lr_pred     <- predict.glm(lr_model,testing,type = 'response')
-lr_cnmatrix <- table(test_label,lr_pred > 0.5)
+lr_model    <- glm(train_data$label ~. ,data=train_data,family = binomial(logit))
+lr_pred     <- predict.glm(lr_model,X_test,type = 'response')
+lr_cnmatrix <- table(y_test,lr_pred > 0.5)
 
 ####Building Random Forest Model#######
-rf_model    <- randomForest(training$label ~ ., data=training,importance=TRUE,ntree=100)
-rf_pred     <- predict(rf_model,testing)
-rf_cnmatrix <- confusionMatrix(table(rf_pred,test_label))
+rf_model    <- randomForest(train_data$label ~ ., data=train_data,importance=TRUE,ntree=100)
+rf_pred     <- predict(rf_model,X_test)
+rf_cnmatrix <- confusionMatrix(table(rf_pred,y_test))
 
 ###Building SVM #####
-svm_model    <- svm(training$label ~ ., data=training)
-svm_pred     <- predict(svm_model,testing)
-svm_cnmatrix <- confusionMatrix(table(svm_pred,test_label))
+svm_model    <- svm(train_data$label ~ ., data=train_data)
+svm_pred     <- predict(svm_model,test_data)
+svm_cnmatrix <- confusionMatrix(table(svm_pred,y_test))
 
 ####Building Decision Tree #####
-dtree_model<-rpart(training$label ~ ., data=training,cp=0.0009)
+dtree_model<-rpart(train_data$label ~ ., data=train_data,cp=0.0009)
 rpart.plot(dtree_model,tweak=1.5)
-dtree_pred <- predict(dtree_model,testing, type = "class")
-dtree_cnmatrix <- confusionMatrix(table(dtree_pred,test_label))
+dtree_pred <- predict(dtree_model,X_test, type = "class")
+dtree_cnmatrix <- confusionMatrix(table(dtree_pred,y_test))
 
 
 ####Oversampling ########
@@ -113,38 +130,40 @@ get_smote_variables <- function(data,col_name){
   perc_under = (1+(minority_class_count/majority_class_count))*100
   return (c(perc_over,perc_under))
 }
-modified_data <- input_data 
-modified_data$new_label[modified_data$label %in% c('dos','rtl','probes','normal')] <- 'others' 
-modified_data$new_label[modified_data$label %in% c('utr')] <- 'utr'
-modified_data <- modified_data[,-which(names(modified_data) %in% c('label'))]
-modified_data$new_label <- as.factor(modified_data$new_label)
-var_list <- get_smote_variables(modified_data,'new_label')
 
 
-######Creating train-test split using stratified sampling########### 
-intot          <- createDataPartition(y = modified_data$new_label, p= 0.7, list = FALSE)
-mod_training   <- as.data.frame(modified_data[intot,])
-mod_testing    <- as.data.frame(modified_data[-intot,])
-mod_test_label <- mod_testing$new_label
-mod_testing    <- mod_testing[-which(names(mod_testing) %in% c('new_label'))]
+#One-class classification for one of the attack types, e.g. utr
+train_data_utr <- train_data 
+train_data_utr$new_label[train_data_utr$label %in% c('dos','rtl','probes','normal')] <- 'not_utr' 
+train_data_utr$new_label[train_data_utr$label %in% c('utr')] <- 'utr'
+train_data_utr <- train_data_utr[,-which(names(train_data_utr) %in% c('label'))]
+train_data_utr$new_label <- as.factor(train_data_utr$new_label)
+smote_var_list <- get_smote_variables(train_data_utr,'new_label')
+
+
+######Modify test data########### 
+test_data_utr <- test_data 
+test_data_utr$new_label[test_data_utr$label %in% c('dos',
+                                       'rtl','probes','normal','others')] <- 'not_utr'
+test_data_utr$new_label[test_data_utr$label %in% c('utr')] <- 'utr'
+test_data_utr <- test_data_utr[,-which(names(test_data_utr) %in% c('label'))]
+test_data_utr$new_label <- as.factor(test_data_utr$new_label)
+X_test_utr                       <- test_data_utr[-which(names(test_data_utr) %in% c('new_label'))]
+y_test_utr                       <- test_data_utr$new_label
+
+
 
 #####Building Naive-bayes Model#########
-mod_nb_model    <- naive_bayes(mod_training$new_label ~ ., data=mod_training)
-mod_nb_pred     <- predict(mod_nb_model,mod_testing)
-mod_nb_cnmatrix <- confusionMatrix(table(mod_nb_pred,mod_test_label))
+mod_nb_model    <- naive_bayes(train_data_utr$new_label ~ ., data=train_data_utr)
+mod_nb_pred     <- predict(mod_nb_model,X_test_utr)
+mod_nb_cnmatrix <- confusionMatrix(table(mod_nb_pred,y_test_utr))
 
 
-smote_modified_data <- SMOTE(modified_data$new_label ~ ., modified_data, perc.over = var_list[1],perc.under=var_list[2])
-
-
-######Creating train-test split using stratified sampling########### 
-intot                <- createDataPartition(y = smote_modified_data$new_label, p= 0.7, list = FALSE)
-smote_mod_training   <- as.data.frame(smote_modified_data[intot,])
-smote_mod_testing    <- as.data.frame(smote_modified_data[-intot,])
-smote_mod_test_label <- smote_mod_testing$new_label
-smote_mod_testing    <- smote_mod_testing[-which(names(smote_mod_testing) %in% c('new_label'))]
+train_data_utr_smote <- SMOTE(train_data_utr$new_label ~ ., train_data_utr, 
+                              perc.over = smote_var_list[1],perc.under=smote_var_list[2])
 
 #####Building Naive-bayes smote_model#########
-smote_mod_nb_model    <- naive_bayes(smote_mod_training$new_label ~ ., data=smote_mod_training)
-smote_mod_nb_pred     <- predict(smote_mod_nb_model,smote_mod_testing)
-smote_mod_nb_cnmatrix <- confusionMatrix(table(smote_mod_nb_pred,smote_mod_test_label))
+smote_mod_nb_model    <- naive_bayes(train_data_utr_smote$new_label ~ ., 
+                                     data=train_data_utr_smote)
+smote_mod_nb_pred     <- predict(smote_mod_nb_model,X_test_utr)
+smote_mod_nb_cnmatrix <- confusionMatrix(table(smote_mod_nb_pred,y_test_utr))
